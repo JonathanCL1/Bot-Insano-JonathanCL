52b339112eacedb84c17c65046a614e5f2d2b69b
JonathanCL1 <108117479+JonathanCL1@users.noreply.github.com>
2022-06-27 09:59:29 -0500

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,7 @@ import { fileURLToPath } from 'url'
 global.owner = [
   ['5218992973056', '✨ JonathanCL - Edicion ✨', true],
   ['5212363041504', 'BotInsano-bot', true],
+  ['5212363041504', 'Bot2JonathanCL',true],
 ] // Cambia los numero que quieras
 global.mods = [] 
 global.prems = [] 
