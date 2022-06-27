0266768e63f5c4daeff59e81120fae73667d825a
JonathanCL1 <108117479+JonathanCL1@users.noreply.github.com>
2022-06-27 09:13:04 -0500

    Update menu-menu_1.js

--- plugins/menu-menu_1.js
+++ plugins/menu-menu_1.js
@@ -1,4 +1,4 @@
-// * 
+/* 
 Codigo abierto - Dejar creditos
 Created by https://github.com/BrunoSobrino 
 👇🏻 EMPEIZA A MODIFICAR DESDE AQUÍ 👇🏻 */
@@ -363,4 +363,4 @@ function clockString(ms) {
 let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
 let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
 let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')} //
+return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')}
