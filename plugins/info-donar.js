60b48dd957f1e5a025984211edaad191a4c3efe5
JonathanCL1 <108117479+JonathanCL1@users.noreply.github.com>
2022-06-27 00:22:08 -0500

    Update info-donar.js

--- plugins/info-donar.js
+++ plugins/info-donar.js
@@ -8,9 +8,8 @@ let donar =`
 *┣ ┅ ━━━━━━━━━ ┅ ━*
 *┃ 𝙷𝙾𝙻𝙰 ${name} 💙*
 *┃*
-*┃ 👉🏻 𝙰𝚀𝚄𝙸 𝙰𝙻𝙶𝚄𝙽𝙾𝚂 𝙳𝙰𝚃𝙾𝚂*
-*┃ 𝙿𝙾𝚁 𝚂𝙸 𝙶𝚄𝚂𝚃𝙰𝚂 𝙰𝙿𝙾𝚈𝙰𝚁 :𝟹*
-*┃*
+*┃ 👉🏻 AQUI ALGUNOS DATOS POR SI GUSTAS APOYAR AL CREADOR DEL CÓDIGO DEL BOT:)*
+*┃ 
 *┃ ➤ 𝙲𝙻𝙰𝚅𝙴:* 
 *┃ 646180192175788481*
 *┃ ➤ 𝙱𝙰𝙽𝙲𝙾: STP* 
