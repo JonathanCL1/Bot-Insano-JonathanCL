f80c911093c49ad09d6d5400dba597fe4dcc751e
JonathanCL1 <108117479+JonathanCL1@users.noreply.github.com>
2022-06-27 08:48:15 -0500

    Update index.js

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ const { name, author } = require(join(__dirname, './package.json'))
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
 
-say('Bot - Insano - JonathanCL', {
+say('Bot Insano JonathanCL', {
 font: 'chrome',
 align: 'center',
 gradient: ['red', 'magenta']})
-say(`Bot creado por Bruno Sobrino`, {
+say(`Bot creado por JonathaCL`, {
 font: 'console',
 align: 'center',
 gradient: ['red', 'magenta']})
