b9c8025959a3f74bd57ea4cb35ed9b02ea91246d
JonathanCL1 <108117479+JonathanCL1@users.noreply.github.com>
2022-06-27 08:07:34 -0500

    Update owner-restart.js

--- plugins/owner-restart.js
+++ plugins/owner-restart.js
@@ -2,7 +2,7 @@ import { spawn } from 'child_process'
 let handler = async (m, { conn, isROwner, text }) => {
     if (!process.send) throw 'Dont: node main.js\nDo: node index.js'
     if (global.conn.user.jid == conn.user.jid) {
-    await m.reply('```R E S T A R T . . .```')
+    await m.reply('```*𝙴𝙻 𝙱𝙾𝚃 𝚂𝙴 𝙴𝚂𝚃𝙰 𝚁𝙴𝙸𝙽𝙸𝙲𝙸𝙰𝙽𝙳𝙾 𝙴𝚂𝙿𝙴𝚁𝙴 𝚄𝙽 𝙿𝙾𝙲𝙾 𝙰𝙽𝚃𝙴𝚂 𝙳𝙴 𝙴𝙽𝚅𝙸𝙰𝚁 𝙼𝙰𝚂 𝙲𝙾𝙼𝙰𝙽𝙳𝙾𝚂*```')
     process.send('reset')
   } else throw '_eeeeeiiittsssss..._'
 }
