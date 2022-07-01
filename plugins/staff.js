b73352e6384cd05a1d4b98c3fe719d0049feffda
JonathanCL1 <108117479+JonathanCL1@users.noreply.github.com>
2022-06-30 21:52:00 -0500

    Create staff.js

---
+++ plugins/staff.js
@@ -0,0 +1,21 @@
+let handler = async (m, { conn, participants, groupMetadata, args }) => {
+const pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => null) || './src/admins.jpg'
+const groupAdmins = participants.filter(p => p.admin)
+const listAdmin = groupAdmins.map((v, i) => `${i + 1}. @${v.id.split('@')[0]}`).join('\n')
+const owner = groupMetadata.owner || groupAdmins.find(p => p.admin === 'superadmin')?.id || m.chat.split`-`[0] + '@s.whatsapp.net'
+let pesan = args.join` `
+let oi = `*𝙼𝙴𝙽𝚂𝙰𝙹𝙴:* ${pesan}`
+let text = `*━「* 𝐈𝐍𝐕𝐎𝐂𝐀𝐍𝐃𝐎 𝐀𝐃𝐌𝐈𝐍𝐒 *」━*
+${oi}
+*STAFF DE:$ {groupMetadata.subject}*
+${listAdmin}
+*𝙲𝚁𝙴𝙰𝙳𝙾𝚁 𝙳𝙴𝙻 𝙶𝚁𝚄𝙿𝙾:* 
+@${owner.split('@')[0]}
+`.trim()
+conn.sendFile(m.chat, pp, 'error.jpg', text, m, false, { mentions: [...groupAdmins.map(v => v.id), owner] })
+}
+handler.help = ['admins <texto>']
+handler.tags = ['group']
+handler.command = /^(admins|@admins|dmins)$/i
+handler.group = true
+export default handler
