db58a5c60db8d1da639c02ed124a631af1913d7d
JonathanCL1 <108117479+JonathanCL1@users.noreply.github.com>
2022-06-30 22:19:54 -0500

    Update staff.js

--- plugins/staff.js
+++ plugins/staff.js
@@ -4,7 +4,6 @@ const groupAdmins = participants.filter(p => p.admin)
 const listAdmin = groupAdmins.map((v, i) => `${i + 1}. @${v.id.split('@')[0]}`).join('\n')
 const owner = groupMetadata.owner || groupAdmins.find(p => p.admin === 'superadmin')?.id || m.chat.split`-`[0] + '@s.whatsapp.net'
 let pesan = args.join` `
-let oi = `*STAFF:* ${pesan}`
 let text = `*━「* 𝗦𝗧𝗔𝗙𝗙 𝗗𝗘 ${groupMetadata.subject} *」━*
 ${oi}
 ${listAdmin}
