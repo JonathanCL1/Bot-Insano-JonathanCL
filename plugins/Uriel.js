2aef38a17851b9c2e2e7129229b30f0037dd196f
JonathanCL1 <108117479+JonathanCL1@users.noreply.github.com>
2022-07-01 18:43:42 -0500

    Update Uriel.js

--- plugins/Uriel.js
+++ plugins/Uriel.js
@@ -1,18 +1,11 @@
 let handler = async (m, { conn, command, usedPrefix }) => {
 let picture = './Uriel.jpg'
-let name = await conn.getName(m.sender)
-let _uptime = process.uptime() * 1000
-let _muptime
-if (process.send) { process.send('uptime')
-_muptime = await new Promise(resolve => { process.once('message', resolve) 
-setTimeout(resolve, 1000) }) * 1000}
-let uptime = clockString(_uptime)
 let estado =`
 Jesús Uriel
 `.trim()
 
 
-handler.help = ['Uriel']
+handler.help = ['estado']
 handler.tags = ['main']
 handler.command = /^(Uriel|Jesus Uriel)$/i
 export default handler
