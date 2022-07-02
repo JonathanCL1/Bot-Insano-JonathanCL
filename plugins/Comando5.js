897938716d5c44ed40beba937bc3efdd583d23d8
JonathanCL1 <108117479+JonathanCL1@users.noreply.github.com>
2022-07-01 19:53:30 -0500

    Update Comando5.js

--- plugins/Comando5.js
+++ plugins/Comando5.js
@@ -1,21 +1,18 @@
-let handler = async (m, { conn, command, usedPrefix }) => {
-let picture = './Uriel.jpg'
-let name = await conn.getName(m.sender)
-let _uptime = process.uptime() * 1000
-let _muptime
-if (process.send) { process.send('uptime')
-_muptime = await new Promise(resolve => { process.once('message', resolve) 
-setTimeout(resolve, 1000) }) * 1000}
-let estado =`
-Jesús uriel
-`.trim()
-
-conn.sendHydrated(m.chat, estado, wm, picture, 'https://github.com/BrunoSobrino/TheMystic-Bot-MD', '𝙶𝙸𝚃𝙷𝚄𝙱', null, null, [
-['𝙼𝙴𝙽𝚄 𝙿𝚁𝙸𝙽𝙲𝙸𝙿𝙰𝙻', '/menu']
-], m)}
-
+import fetch from 'node-fetch'
+let handler = async (m, { conn, command }) => {
+let url = pack[Math.floor(Math.random() * pack.length)]
+conn.sendButton(m.chat, `_Jesus Uriel_`, author, url, [['👻 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 👻', `/${command}`]], m)
+}
 handler.customPrefix = /Uriel|uriel|Jesus Uriel|jesus uriel/i
 handler.command = new RegExp
 handler.fail = null
 handler.exp = 100
 export default handler
+
+global.pack = [
+  "https://telegra.ph/file/0cfa9a0d6d118badece2d.jpg",
+  "https://telegra.ph/file/0127ce2d867ece9249c3d.jpg",
+  "https://telegra.ph/file/a731c6b8e5d797e47830f.jpg",
+  "https://telegra.ph/file/f36607b51a79a3cbce489.jpg",
+  "https://telegra.ph/file/a99c75a664b2fb96e7199.jpg",
+ ]
