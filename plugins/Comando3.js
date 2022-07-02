410ceee7b349c792c784014c6c7c2feda72a95d9
JonathanCL1 <108117479+JonathanCL1@users.noreply.github.com>
2022-07-02 17:18:20 -0500

    Update Comando3.js

--- plugins/Comando3.js
+++ plugins/Comando3.js
@@ -5,7 +5,7 @@ react: {
 text: emot,
 key: m.key
 }})}
-handler.customPrefix = /(johan|johan3|botpolka|Botpolka|polka|Noe|Noe|abdiel|Juan|Juanito)/i
+handler.customPrefix = /(johan|johan3|botpolka|Botpolka|polka|Noe|Noe|abdiel|Juan|Juanito|angel|angeltovar)/i
 handler.command = new RegExp
 export default handler
 function pickRandom(list) {
