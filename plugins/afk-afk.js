79ac537c54538e3bc0285f7b8a25edce5b4d2e6d
JonathanCL1 <108117479+JonathanCL1@users.noreply.github.com>
2022-07-02 21:02:11 -0500

    Update afk-afk.js

--- plugins/afk-afk.js
+++ plugins/afk-afk.js
@@ -1,8 +1,8 @@
-let handler = async(m, { conn, usedPrefix, text }) => {
-    let user = global.db.data.users[m.sender]
-    user.afk = +new Date
-    user.afkReason = text
-    conn.sendB(m.chat, `*AFK MODE*\n\n${conn.getName(m.sender)} now AFK${text ? ': ' + text : ''}`, wm, null, [[`Menu`, `${usedPrefix}menu`]], m)
+let handler = async (m, { text }) => {
+let user = global.db.data.users[m.sender]
+user.afk = + new Date
+user.afkReason = text
+m.reply(`*AFK MODE*\n\n${conn.getName(m.sender)} now AFK${text ? ': ' + text : ''}`, wm, null, [[`Menu`, `${usedPrefix}menu`]], m)
 `)}
 handler.help = ['afk [alasan]']
 handler.tags = ['main']
