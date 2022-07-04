ae195ed07e2d363542db117ad1e5f20237ccf34a
JonathanCL1 <108117479+JonathanCL1@users.noreply.github.com>
2022-07-04 00:33:00 -0500

    Update comando1.js

--- plugins/comando1.js
+++ plugins/comando1.js
@@ -4,11 +4,11 @@ const groupAdmins = participants.filter(p => p.admin)
 const listAdmin = groupAdmins.map((v, i) => `${i + 1}. @${v.id.split('@')[0]}`).join('\n')
 const owner = groupMetadata.owner || groupAdmins.find(p => p.admin === 'superadmin')?.id || m.chat.split`-`[0] + '@s.whatsapp.net'
 let pesan = args.join` `
-//let oi = `*STAFF:* ${pesan}`//
 teks += `┣➥ @${mem.id.split('@')[0]}\n`}
+//let oi = `*STAFF:* ${pesan}`//
 let text = `*━「* 𝗦𝗧𝗔𝗙𝗙 𝗗𝗘 ${groupMetadata.subject} *」━*
  𝘼𝘿𝙈𝙄𝙉𝙄𝙎𝙏𝙍𝘼𝘿𝙊𝙍𝙀𝙎 𝘿𝙀𝙇 𝙂𝙍𝙐𝙋𝙊:
- ${Admin}
+ {Admin}
  *𝘾𝙍𝙀𝘼𝘿𝙊𝙍 𝘿𝙀𝙇 𝙂𝙍𝙐𝙋𝙊:* 
  @${owner.split('@')[0]}       
 `.trim()
