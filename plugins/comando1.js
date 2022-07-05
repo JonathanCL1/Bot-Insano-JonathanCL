8015ff5dba4be4620e807bac3b6d225761837949
JonathanCL1 <108117479+JonathanCL1@users.noreply.github.com>
2022-07-04 21:58:19 -0500

    Update comando1.js

--- plugins/comando1.js
+++ plugins/comando1.js
@@ -11,10 +11,6 @@ let text = `*━「* 𝗦𝗧𝗔𝗙𝗙 𝗗𝗘 ${groupMetadata.subject} *」
  *𝘾𝙍𝙀𝘼𝘿𝙊𝙍 𝘿𝙀𝙇 𝙂𝙍𝙐𝙋𝙊:* 
  @${owner.split('@')[0]}       
 `.trim()
-conn.sendHydrated2(m.chat, str, wm, pp, 'https://www.paypal.me/JonathanCL1', '𝙿𝙰𝚈𝙿𝙰𝙻', 'https://github.com/JonathanCL1/Bot-Insano-JonathanCL', '𝙶𝙸𝚃𝙷𝚄𝙱', [
-['🌹 𝙾𝚆𝙽𝙴𝚁 🌹', '/owner'],
-['🐾 𝙸𝙽𝙵𝙾𝙱𝙾𝚃 🐾', '/infobot']
-], m,)
 conn.sendFile(m.chat, pp, 'error.jpg', text, m, false, { mentions: [...groupAdmins.map(v => v.id), owner] })
 }
 handler.help = ['admins <texto>']
