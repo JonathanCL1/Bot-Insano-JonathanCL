64a098bcb6884ca47c65fb612012f9ef39f546b8
JonathanCL1 <108117479+JonathanCL1@users.noreply.github.com>
2022-07-04 22:34:34 -0500

    Update info-estado.js

--- plugins/info-estado.js
+++ plugins/info-estado.js
@@ -1,4 +1,4 @@
-//let handler = async (m, { conn, command, usedPrefix }) => {
+let handler = async (m, { conn, command, usedPrefix }) => {
 let picture = './Menu2.jpg'
 let name = await conn.getName(m.sender)
 let _uptime = process.uptime() * 1000
@@ -32,4 +32,4 @@ function clockString(ms) {
 let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
 let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
 let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')}//
+return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')}
