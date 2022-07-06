71a8da487ec06eafcfd3dd7758001f41e3b1518e
JonathanCL1 <108117479+JonathanCL1@users.noreply.github.com>
2022-07-06 14:12:49 -0500

    Update Comando4.js

--- plugins/Comando4.js
+++ plugins/Comando4.js
@@ -1,12 +1,12 @@
-let handler = async (m, { conn }) => {
-let emot = pickRandom(["🌟", "🎇", "😍","🥵"])
-conn.sendMessage(m.chat, {
-react: {
-text: emot,
-key: m.key
-}})}
-handler.customPrefix = /(Bot insano|bot insano)/i
-handler.command = new RegExp
-export default handler
-function pickRandom(list) {
-return list[Math.floor(Math.random() * list.length)]}
+//let handler = async (m, { conn }) => {
+//let emot = pickRandom(["🌟", "🎇", "😍","🥵"])
+//conn.sendMessage(m.chat, {
+//react: {
+//text: emot,
+//key: m.key
+//}})}
+//handler.customPrefix = /(Bot insano|bot insano)/i
+//handler.command = new RegExp
+//export default handler
+//function pickRandom(list) {
+//return list[Math.floor(Math.random() * list.length)]}
