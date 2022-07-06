5e2c07a5f8d0e675351bc2c085faf48bae61d923
JonathanCL1 <108117479+JonathanCL1@users.noreply.github.com>
2022-07-06 00:35:24 -0500

    Update Comando3.js

--- plugins/Comando3.js
+++ plugins/Comando3.js
@@ -1,12 +1,1 @@
-// let handler = async (m, { conn }) => {
-let emot = pickRandom(["🐀", "🐀", "🐭","🧀"])
-conn.sendMessage(m.chat, {
-react: {
-text: emot,
-key: m.key
-}})}
-handler.customPrefix = /(johan|johan3|botpolka|Botpolka|polka|Noe|Noe|abdiel|Juan|Juanito|angel|angeltovar|Damian|damian)/i
-handler.command = new RegExp
-export default handler
-function pickRandom(list) {
-return list[Math.floor(Math.random() * list.length)]} //
+//comando desactivado
