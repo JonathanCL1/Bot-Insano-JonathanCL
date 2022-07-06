94c0ffdd692005936d985a70819ecf28aebf7fb1
JonathanCL1 <108117479+JonathanCL1@users.noreply.github.com>
2022-07-06 14:08:20 -0500

    Update Comando5.js

--- plugins/Comando5.js
+++ plugins/Comando5.js
@@ -1,19 +1,19 @@
 // import fetch from 'node-fetch'
-let handler = async (m, { conn, command }) => {
-let url = JonathanCL[Math.floor(Math.random() * JonathanCL.length)]
-conn.sendButton(m.chat, `_JonathanCL_`, author, url, [['𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴', `/${handler.customPrefix = /JonathanCL|Jonathan|jonathan/i}`]], m)
-}
-handler.customPrefix = /JonathanCL|Jonathan|jonathan/i
-handler.command = new RegExp
-handler.fail = null
-handler.exp = 100
-export default handler
+//let handler = async (m, { conn, command }) => {
+//let url = JonathanCL[Math.floor(Math.random() * JonathanCL.length)]
+//conn.sendButton(m.chat, `_JonathanCL_`, author, url, [['𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴', `/${handler.customPrefix = /JonathanCL|Jonathan|jonathan/i}`]], m)
+//}
+//handler.customPrefix = /JonathanCL|Jonathan|jonathan/i
+//handler.command = new RegExp
+//handler.fail = null
+//handler.exp = 100
+//export default handler
 
-global.JonathanCL = [
-  "https://telegra.ph/file/5ea4f5083b4f4e1fb4b1a.png",
-  "https://telegra.ph/file/4e26bb46fea53763af647.png",
-  "https://telegra.ph/file/5761836e45d77256eeac8.png",
-  "https://telegra.ph/file/2b5d21a489488582bf7d3.png",
-  "https://telegra.ph/file/cb64c6506ebf70bd79653.jpg",
-]
-                                      //
+//global.JonathanCL = [
+//  "https://telegra.ph/file/5ea4f5083b4f4e1fb4b1a.png",
+//  "https://telegra.ph/file/4e26bb46fea53763af647.png",
+// "https://telegra.ph/file/5761836e45d77256eeac8.png",
+//  "https://telegra.ph/file/2b5d21a489488582bf7d3.png",
+//  "https://telegra.ph/file/cb64c6506ebf70bd79653.jpg",
+//]
+                                      
