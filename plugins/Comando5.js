581205c4681c01e210489e9de23385c2c70b6f01
JonathanCL1 <108117479+JonathanCL1@users.noreply.github.com>
2022-07-05 23:04:01 -0500

    Update Comando5.js

--- plugins/Comando5.js
+++ plugins/Comando5.js
@@ -1,8 +1,9 @@
 import fetch from 'node-fetch'
 let handler = async (m, { conn, command }) => {
-let url = pack[Math.floor(Math.random() * pack.length)]
+let url = uriel[Math.floor(Math.random() * uriel.length)]
+conn.sendButton(m.chat, `_JonathanCL_`, author, url, [['🛐 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 🛐', `/${command}`]], m)
 }
-handler.customPrefix = /^(jonathancl|JonathanCL|Jonathan|jonathan)$/i
+handler.customPrefix = /jonathancl|JonathanCL|Jonathan|jonathan/i
 handler.command = new RegExp
 handler.fail = null
 handler.exp = 100
@@ -11,4 +12,7 @@ export default handler
 global.pack = [
   "https://telegra.ph/file/5ea4f5083b4f4e1fb4b1a.png",
   "https://telegra.ph/file/4e26bb46fea53763af647.png",
+  "https://telegra.ph/file/5761836e45d77256eeac8.png",
+  "https://telegra.ph/file/2b5d21a489488582bf7d3.png",
+  "https://telegra.ph/file/cb64c6506ebf70bd79653.jpg",
 ]
