70b94fb320a1640c92645b7ea8c175e1fada7332
JonathanCL1 <108117479+JonathanCL1@users.noreply.github.com>
2022-07-06 14:51:16 -0500

    Create Comando9.js

---
+++ plugins/Comando9.js
@@ -0,0 +1,20 @@
+import fetch from 'node-fetch'
+let handler = async (m, { conn, command }) => {
+let url = Juanito[Math.floor(Math.random() * Juanito.length)]
+conn.sendButton(m.chat, `_Juan poca insanidad_`, author, url, [['𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴', `/${handler.customPrefix = /Juan|juanito|Juanito|juan/i}`]], m)
+}
+handler.customPrefix = /Juan|juanito|Juanito|juan/i
+handler.command = new RegExp
+handler.fail = null
+handler.exp = 100
+export default handler
+
+global.Juanito = [
+    "https://telegra.ph/file/5873c51aef29db98dbd73.jpg"
+    "https://telegra.ph/file/79f096456487e543ec549.jpg"
+    "https://telegra.ph/file/d3db4266a5334783fe4b5.jpg"
+    "https://telegra.ph/file/31ba285539aa5b1a7d274.jpg"
+    "https://telegra.ph/file/01557cb8027d6febc80df.jpg"
+    "https://telegra.ph/file/b15b79b1069897bdd99e6.jpg"
+    "https://telegra.ph/file/59512e99fdb67a9c06648.jpg"
+    "https://telegra.ph/file/423b86cc4d259a825a072.jpg"
