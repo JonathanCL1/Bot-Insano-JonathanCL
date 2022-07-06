67b0db6c55238928014035f099fe4786f9c661ac
JonathanCL1 <108117479+JonathanCL1@users.noreply.github.com>
2022-07-06 15:03:01 -0500

    Update Comando9.js

--- plugins/Comando9.js
+++ plugins/Comando9.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch'
 let handler = async (m, { conn, command }) => {
 let url = Juanito[Math.floor(Math.random() * Juanito.length)]
-conn.sendButton(m.chat, `_Juan Poco insano lorenzo_`, author, url, [['𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴', `/${handler.customPrefix = /Juan|Juanito|juanito|juan/i}`]], m)
+conn.sendButton(m.chat, `_Juanitopocoinsano_`, author, url, [['𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴', `/${handler.customPrefix = /Juan|Juanito|juanito|juan/i}`]], m)
 }
 handler.customPrefix = /Juan|Juanito|juanito|juan/i
 handler.command = new RegExp
@@ -15,6 +15,7 @@ global.Juanito = [
   "https://telegra.ph/file/31ba285539aa5b1a7d274.jpg"
   "https://telegra.ph/file/01557cb8027d6febc80df.jpg"
   "https://telegra.ph/file/b15b79b1069897bdd99e6.jpg"
+  "https://telegra.ph/file/79f096456487e543ec549.jpg"
   "https://telegra.ph/file/59512e99fdb67a9c06648.jpg"
   "https://telegra.ph/file/423b86cc4d259a825a072.jpg"
 ]
